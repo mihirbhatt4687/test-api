6cd1c494f6de858bae195d2aa359c12ac616c828
Mihir <mihirbhatt4687@gmail.com>
2024-03-31 15:39:53 +0530

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,7 +69,7 @@
                                 </div>
 
                                 <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Laracasts</h2>
-<h1>change 123</h1>
+<h1>change</h1>
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
                                     Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
                                 </p>
